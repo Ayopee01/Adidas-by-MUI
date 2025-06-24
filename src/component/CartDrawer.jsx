4338a46244a640ee3fc7d0aa6162935bde2e9485
Ayopee01 <Ayopee001@gmail.com>
2025-06-24 19:54:56 +0700

    fix

--- src/component/CartDrawer.jsx
+++ src/component/CartDrawer.jsx
@@ -140,7 +140,7 @@ const CartDrawer = ({ open, onClose }) => {
         PaperProps={{
           sx: {
             top: `${CART_DRAWER_HEIGHT}px`,
-            height: `calc(80vh - ${CART_DRAWER_HEIGHT}px)`,
+            height: `calc(85vh - ${CART_DRAWER_HEIGHT}px)`,
             width: 370,
             maxWidth: "95vw",
             borderTopLeftRadius: 20,
